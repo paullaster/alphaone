ce71ed123cfc11a9dd8895ad1046623d88632a46
paullaster <paullasterokoth98@gmail.com>
2024-02-06 10:23:02 +0300

    scafold alphaone project

--- src/packages/Landing/components/GetStarted.vue
+++ src/packages/Landing/components/GetStarted.vue
@@ -1,9 +1,33 @@
 <template>
-    <v-responsive>
-        <v-card>
-            <v-card-text>getstarted</v-card-text>
-        </v-card>
-    </v-responsive>
+    <v-card elevation="2" color="#000080" height="100vh" class="d-flex justify-center align-center px-2">
+        <v-card-items></v-card-items>
+        <v-row>
+            <v-col cols="12">
+                <v-form>
+                    <v-row>
+                        <v-col cols="12">
+                            <v-text-field label="Email address" placeholder="johndoe@gmail.com" type="email"></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-text-field label="Password" type="input"
+                                hint="Enter your password to access this site"></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-btn block class="transparent">
+                                Login
+                            </v-btn>
+                        </v-col>
+                    </v-row>
+                </v-form>
+            </v-col>
+            <v-cols class="d-flex" plain>
+                <v-card-text>New here?</v-card-text>
+                <v-btn>
+                    create account
+                </v-btn>
+            </v-cols>
+        </v-row>
+    </v-card>
 </template>
 
 <script>
@@ -13,6 +37,4 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/packages/Landing/routes.js
+++ src/packages/Landing/routes.js
@@ -1,16 +1,20 @@
 import LandingComponent from './Views/LandingComponent.vue';
 import GetStarted  from './components/GetStarted.vue';
 
-export default [
-    {
-        path: '/landing',
-        name: 'landing',
-        component: LandingComponent
-    },
-    {
-        path: '/getstarted',
-        name: 'getstarted',
-        component: GetStarted
-    }
+export default {
+    path:'',
+    name: 'landing',
+    redirect: '/landing',
+    children: [
+        {
+            path: '/landing',
+            component: LandingComponent
+        },
+        {
+            path: '/getstarted',
+            name: 'getstarted',
+            component: GetStarted
+        }
+    ]
 
-]
+}
