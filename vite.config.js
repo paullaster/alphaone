7242f2dee611674149b676d6f9798ff077a6ae40
paullaster <paullasterokoth98@gmail.com>
2024-02-06 03:40:15 +0300

    scafold alphaone project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  envPrefix: "VUE_APP",
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url))
+		}
+	}
 })
