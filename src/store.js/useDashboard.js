7084593f96010daf04d7adfb3cdb62c12b84c669
paullaster <paullasterokoth98@gmail.com>
2024-02-15 21:32:51 +0300

    update ui

--- src/store.js/useDashboard.js
+++ src/store.js/useDashboard.js
@@ -3,7 +3,10 @@ import { defineStore } from "pinia";
 export const useDashboardStore = defineStore('dashboard', {
     state() {
         return {
-            
+
         }
+    },
+    getters: {
+        dashboardGetter: (state) => (key) => state[key]
     }
 });
