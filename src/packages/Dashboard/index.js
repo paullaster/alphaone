8c9426a4a313c8e5cccb5b546c4b1e5c2b185bdf
paullaster <paullasterokoth98@gmail.com>
2024-02-15 21:41:35 +0300

    update ui

--- src/packages/Dashboard/index.js
+++ src/packages/Dashboard/index.js
@@ -1,8 +1,11 @@
 import routes from "./routes";
 import { addLink } from "@/utils/addSidebarLinks";
+import links from "./links";
 
 export default {
     install(app, option) {
         option.router.addRoute(routes);
+
+        addLink(links);
     }
 }
