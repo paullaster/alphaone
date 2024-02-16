aab4d31ab373db97aac240e5909809879f53c78c
paullaster <paullasterokoth98@gmail.com>
2024-02-16 15:15:02 +0300

    update dashboard ui

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -1,28 +1,42 @@
 <template>
-    <v-card class="card-custom">
-        <v-layout>
-            <v-app-bar
-            color="teal"
-            >
-            <template v-slot:prepend>
-          <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        </template>
-            </v-app-bar>
-        </v-layout>
+    <v-card
+      class=" custom-height mx-auto"
+
+    >
+      <v-layout>
+        <v-app-bar
+          color="primary"
+          density="compact"
+        >
+          <template v-slot:prepend>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+          </template>
+  
+          <v-app-bar-title>{{ APPNAME }}</v-app-bar-title>
+  
+          <template v-slot:append>
+            <v-btn icon="mdi-dots-vertical"></v-btn>
+          </template>
+        </v-app-bar>
+  
+        <v-main>
+          <v-container fluid>
+            <p>Dashboard</p>
+          </v-container>
+        </v-main>
+      </v-layout>
     </v-card>
-</template>
+  </template>
 
 <script>
-
-import { Auth } from '@/utils';
-    export default {
-        name: "DashboardLayout",
-    }
+import { APPNAME } from '../environment';
+export default {
+    name: "DashboardLayout",
+}
 </script>
 
 <style lang="scss" scoped>
-.card-custom {
-    background-color: red;
+.custom-height {
     height: 100dvh;
 }
 </style>
