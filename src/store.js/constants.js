80f59c60a86fcea73bfc821955e4f4820db9df1a
paullaster <paullasterokoth98@gmail.com>
2024-02-17 14:44:10 +0300

    update ui

--- src/store.js/constants.js
+++ src/store.js/constants.js
@@ -2,4 +2,7 @@ export default {
     login: 'users/auth/login',
     sign:  'users/auth/signup',
     createUser: 'users/auth/create',
+
+    // COURSES
+    courses: 'courses',
 }
--- src/store.js/useDashboard.js
+++ src/store.js/useDashboard.js
@@ -1,11 +1,13 @@
 import { defineStore } from "pinia";
 import { _request } from '@/utils';
 import { useGlobalStore } from "./useGlobal";
+import constants from "./constants";
 
 export const useDashboardStore = defineStore('dashboard', {
     state() {
         return {
             links: [],
+            courses: [],
         }
     },
     getters: {
@@ -27,7 +29,22 @@ export const useDashboardStore = defineStore('dashboard', {
 			}
 		},
         courses(params){
-            
+            this.setDashboardLoader(true);
+            _request({
+                url: constants.courses,
+                method: "GET",
+                params
+            })
+            then((res) => {
+                this.$patch({
+                    courses: res.data.rows
+                });
+                this.setDashboardLoader(false);
+            })
+            .catch((error) => {
+                this.setDashboardLoader(false);
+                this.toast.error(error?.message);
+            });
         }
 
     }
