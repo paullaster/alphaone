57c2cecceb18aeeead0ca9e765215ee08857ecb4
paullaster <paullasterokoth98@gmail.com>
2024-02-17 16:36:05 +0300

    update ui

--- src/packages/Dashboard/views/MainComponent.vue
+++ src/packages/Dashboard/views/MainComponent.vue
@@ -2,7 +2,7 @@
 import { useDashboardStore } from '@/store.js';
 export default {
     name: "MainDashboard",
-    beforeRouteEnter (to, from, next) {
+    beforeRouteEnter(to, from, next) {
         next((v) => {
             v.dashboardStore.coursesList();
         });
@@ -11,12 +11,29 @@ export default {
         const dashboardStore = useDashboardStore();
         return { dashboardStore };
     },
+    mounted() {
+        console.log(this.$vuetify.display);
+    },
     computed: {
         coursesList() {
             return this.dashboardStore.dashboardGetter('courses');
         },
         cols() {
-            console.log(this.$vuetify.display);
+            const { lg, md, sm } = this.$vuetify.display;
+            console.log(`lg ${lg} : md ${md} : sm ${sm}`);
+            let cl = [];
+            if (lg) {
+                cl = [4, 4, 4]
+            }
+            if (md) {
+                cl = [6, 6]
+            }
+            cl = [12]
+            return cl;
+
+        },
+        loading() {
+            return !!this.coursesList;
         }
     }
 }
@@ -25,12 +42,17 @@ export default {
 
 <template>
     <v-card>
-        <v-row  class="mb-6">
-            <v-col v-for="course in coursesList" :key="course.id" cols="12">{{ course.name }}</v-col>
+        <v-row class="mb-6">
+            <v-col v-for="course in coursesList" :key="course.id" :cols="cols[0]">
+                <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+                    <template v-slot:loader="{ isActive }">
+                        <v-progress-linear :active="isActive" color="deep-purple" height="4"
+                            indeterminate></v-progress-linear>
+                    </template>
+                </v-card>
+            </v-col>
         </v-row>
     </v-card>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
