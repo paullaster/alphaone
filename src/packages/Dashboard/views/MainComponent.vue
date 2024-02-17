12aaac756a05b2e5e7c195d939720fbabe7e2fc6
paullaster <paullasterokoth98@gmail.com>
2024-02-17 16:51:54 +0300

    update ui

--- src/packages/Dashboard/views/MainComponent.vue
+++ src/packages/Dashboard/views/MainComponent.vue
@@ -1,5 +1,6 @@
 <script>
 import { useDashboardStore } from '@/store.js';
+import {APPNAME} from '@/environment';
 export default {
     name: "MainDashboard",
     beforeRouteEnter(to, from, next) {
@@ -10,6 +11,11 @@ export default {
     setup() {
         const dashboardStore = useDashboardStore();
         return { dashboardStore };
+    },
+    data(){
+        return {
+            APPNAME,
+        }
     },
     mounted() {
         console.log(this.$vuetify.display);
@@ -41,31 +47,43 @@ export default {
 </script>
 
 <template>
-        <v-card>
-            <v-row class="mb-6">
-                <v-col v-for="course in coursesList" :key="course.id" :cols="cols[0]">
-                    <v-card :loading="loading" class="mx-auto my-12" max-width="374">
-                        <template v-slot:loader="{ isActive }">
-                            <v-progress-linear :active="isActive" color="deep-purple" height="4"
-                                indeterminate></v-progress-linear>
-                        </template>
-                        <v-img cover height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
-                        <v-card-item>
-                            <v-card-title>{{ course.name }}</v-card-title>
+    <v-card>
+        <v-row class="mb-6">
+            <v-col v-for="course in coursesList" :key="course.id" :cols="cols[0]">
+                <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+                    <template v-slot:loader="{ isActive }">
+                        <v-progress-linear :active="isActive" color="deep-purple" height="4"
+                            indeterminate></v-progress-linear>
+                    </template>
+                    <v-img cover height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
+                    <v-card-item>
+                        <v-card-title>{{ course.name }}</v-card-title>
 
-                            <v-card-subtitle>
-                                <span class="me-1">Local Favorite</span>
+                        <v-card-subtitle>
+                            <span class="me-1">Eldoret</span>
 
-                                <v-icon color="error" icon="mdi-fire-circle" size="small"></v-icon>
-                            </v-card-subtitle>
-                        </v-card-item>
+                            <v-icon color="error" icon="mdi-map-marker" size="small"></v-icon>
+                        </v-card-subtitle>
+                    </v-card-item>
+                    <v-card-text>
+                        <v-row align="center" class="mx-0">
+                            <v-rating :model-value="4.5" color="amber" density="compact" half-increments readonly
+                                size="small"></v-rating>
+
+                            <div class="text-grey ms-4">
+                                4.5 (2)
+                            </div>
+                        </v-row>
+                        <div class="my-4 text-subtitle-1">
+                            $ • {{ APPNAME }}, School
+                        </div>
+
+                        <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio
+                            seating.</div>
+                    </v-card-text>
                 </v-card>
             </v-col>
         </v-row>
-    </v-card>
-</template>
-
-<style lang="scss" scoped></style>
     </v-card>
 </template>
 
