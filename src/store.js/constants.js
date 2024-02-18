8d298b92b9914165069cc36c146b376d6c1ebb2f
paullaster <paullasterokoth98@gmail.com>
2024-02-18 20:12:15 +0300

    update ui

--- src/store.js/constants.js
+++ src/store.js/constants.js
@@ -5,4 +5,7 @@ export default {
 
     // COURSES
     courses: 'courses',
+
+    // IMAGES
+    images: 'images',
 }
--- src/store.js/useSetup.js
+++ src/store.js/useSetup.js
@@ -1,12 +1,19 @@
 import { defineStore } from "pinia";
+import { _request } from "@/http";
+import constants from "./constants";
 
 export const useSetupStore = defineStore('setup', {
     state: () => {
         return {
-
+            images: [],
         }
     },
     getters: {
         setGetters: (state) => (key) => state[key],
+    },
+    actions: {
+        getImages(params) {
+            
+        }
     }
 });
