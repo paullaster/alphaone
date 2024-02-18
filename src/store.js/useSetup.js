9a3dc86b23713b0fd377d21833c090e6e755a524
paullaster <paullasterokoth98@gmail.com>
2024-02-18 19:55:31 +0300

    update ui

--- src/store.js/useSetup.js
+++ src/store.js/useSetup.js
@@ -3,7 +3,10 @@ import { defineStore } from "pinia";
 export const useSetupStore = defineStore('setup', {
     state: () => {
         return {
-            
+
         }
+    },
+    getters: {
+        setGetters: (state) => (key) => state[key],
     }
 });
