095d4edb6a80e0ac894c2569d662935a77acf7e6
paullaster <paullasterokoth98@gmail.com>
2024-02-18 23:45:00 +0300

    update ui

--- src/packages/Dashboard/components/ApplicationComponent.vue
+++ src/packages/Dashboard/components/ApplicationComponent.vue
@@ -1,38 +1,87 @@
 <template>
-    <div>
-        application
-    </div>
+    <v-container>
+        <v-card>
+            <v-form>
+                <v-row>
+                    <v-col cols="6">
+                        <v-text-field v-model="formData.name" 
+                            label="Full name"></v-text-field>
+                    </v-col>
+                    <v-col cols="6">
+                        <v-text-field v-model="formData.email" 
+                            label="Email"></v-text-field>
+                    </v-col>
+                    <v-col cols="6">
+                        <v-text-field v-model="formData.identificationNumber" 
+                            label="Identification document number"></v-text-field>
+                    </v-col>
+                    <v-col cols="6">
+                        <v-text-field v-model="formData.age" 
+                            label="Age"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field v-model="formData.gender" 
+                            label="Gender"></v-text-field>
+                    </v-col>
+                    <!-- <v-col>
+                        <v-text-field v-model="name.value.value" :counter="10" :error-messages="name.errorMessage.value"
+                            label="Name"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field v-model="name.value.value" :counter="10" :error-messages="name.errorMessage.value"
+                            label="Name"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field v-model="name.value.value" :counter="10" :error-messages="name.errorMessage.value"
+                            label="Name"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field v-model="name.value.value" :counter="10" :error-messages="name.errorMessage.value"
+                            label="Name"></v-text-field>
+                    </v-col> -->
+                </v-row>
+            </v-form>
+        </v-card>
+    </v-container>
 </template>
 
 <script>
-    import { useDashboardStore, useSetupStore } from '@/store.js';
-    export default {
-        name: 'ApplicationComponent',
-        beforeRouteEnter (to, from, next) {
-            next((v) => {
-                v.dashboardStore.getCourse({id: v.$route.params.course});
-                v.setupStore.fetchSingleImage({sourceID: btoa(v.$route.params.course), documentType: 'Course'});
-            });
-        },
-        setup() {
-            const dashboardStore = useDashboardStore();
-            const setupStore = useSetupStore();
-            return {
-                dashboardStore,
-                setupStore,
-            }
-        },
-        computed: {
-            course() {
-                return this.dashboardStore.dashboardGetter('course');
-            },
-            image() {
-                return this.setupStore.setupGetters('image');
+import { useDashboardStore, useSetupStore } from '@/store.js';
+export default {
+    name: 'ApplicationComponent',
+    beforeRouteEnter(to, from, next) {
+        next((v) => {
+            v.dashboardStore.getCourse({ id: v.$route.params.course });
+            v.setupStore.fetchSingleImage({ sourceID: btoa(v.$route.params.course), documentType: 'Course' });
+        });
+    },
+    setup() {
+        const dashboardStore = useDashboardStore();
+        const setupStore = useSetupStore();
+        return {
+            dashboardStore,
+            setupStore,
+        }
+    },
+    data(){
+        return {
+            formData: {
+                name: '',
+                email: '',
+                identificationNumber: '',
+                age: '',
             }
         }
+    },
+    computed: {
+        course() {
+            return this.dashboardStore.dashboardGetter('course');
+        },
+        image() {
+            return this.setupStore.setupGetters('image');
+        },
     }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
