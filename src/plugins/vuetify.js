5090bef260fe5dc689acbbd32caeda938291b122
paullaster <paullasterokoth98@gmail.com>
2024-02-24 19:55:01 +0300

    add user payment options

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,12 +2,28 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import '@mdi/font/css/materialdesignicons.css' 
+import '@mdi/font/css/materialdesignicons.css'
 
 export default createVuetify({
   components,
   directives,
   icons: {
     defaultSet: 'mdi',
+  },
+  theme: {
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          primary: '#00C853',
+          secondary: '#424242',
+          accent: '#82b1ff',
+          error: '#b71c1c',
+          info: '#2196f3',
+          success: '#4caf50',
+          danger: '#ff5722',
+        }
+      },
+    }
   },
 });
