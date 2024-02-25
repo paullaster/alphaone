3129f0e390057f9fbde6602c41daeb0156c19ed6
paullaster <paullasterokoth98@gmail.com>
2024-02-25 05:14:08 +0300

    add user payment options

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -1,15 +1,15 @@
 <template>
   <v-card class="custom-height mx-auto">
     <v-layout>
-      <v-app-bar color="primary" density="compact">
+      <v-app-bar color="primary" density="compact" class="customAppBarStyles">
         <template v-slot:prepend>
           <v-app-bar-nav-icon @click="toggleSidebar"></v-app-bar-nav-icon>
         </template>
 
-        <v-app-bar-title color="customwhite">{{ APPNAME }}</v-app-bar-title>
+        <v-app-bar-title >{{ APPNAME }}</v-app-bar-title>
 
         <template v-slot:append>
-          <v-btn icon="mdi-dots-vertical"></v-btn>
+          <v-btn icon="mdi-dots-vertical" color="customwhite"></v-btn>
         </template>
       </v-app-bar>
       <SidebarDrawer v-show="viewSidebar" />
@@ -45,5 +45,10 @@ export default {
 <style lang="scss" scoped>
 .custom-height {
   min-height: 100dvh;
+
+  .customAppBarStyles {
+    height: 8.8dvh;
+    position: fixed !important;
+  }
 }
 </style>
