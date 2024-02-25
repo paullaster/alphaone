ef80a46e5eb0696df4fa075f72a2bc92b187c066
paullaster <paullasterokoth98@gmail.com>
2024-02-25 05:06:45 +0300

    add user payment options

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -1,57 +1,49 @@
 <template>
-    <v-card
-      class=" custom-height mx-auto"
+  <v-card class="custom-height mx-auto">
+    <v-layout>
+      <v-app-bar color="primary" density="compact">
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon @click="toggleSidebar"></v-app-bar-nav-icon>
+        </template>
 
-    >
-      <v-layout>
-          <v-app-bar
-          color="primary"
-          density="compact"
-          >
-          <template v-slot:prepend>
-            <v-app-bar-nav-icon @click="toggleSidebar"></v-app-bar-nav-icon>
-          </template>
-  
-          <v-app-bar-title>{{ APPNAME }}</v-app-bar-title>
-  
-          <template v-slot:append>
-              <v-btn icon="mdi-dots-vertical"></v-btn>
-            </template>
-        </v-app-bar>
-        <SidebarDrawer v-show="viewSidebar"/>
-        <v-main>
-          <v-container fluid>
-            <router-view name="view"></router-view>
-        </v-container>
-    </v-main>
-</v-layout>
-    </v-card>
-  </template>
+        <v-app-bar-title color="customwhite">{{ APPNAME }}</v-app-bar-title>
+
+        <template v-slot:append>
+          <v-btn icon="mdi-dots-vertical"></v-btn>
+        </template>
+      </v-app-bar>
+      <SidebarDrawer v-show="viewSidebar" />
+      <v-main>
+        <router-view name="view"></router-view>
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
 
 <script>
-import { APPNAME } from '../environment';
-import SidebarDrawer from '../components/SidebarComponent.vue';
+import { APPNAME } from "../environment";
+import SidebarDrawer from "../components/SidebarComponent.vue";
 export default {
-    name: "DashboardLayout",
-    components: {
-        SidebarDrawer
+  name: "DashboardLayout",
+  components: {
+    SidebarDrawer,
+  },
+  data() {
+    return {
+      APPNAME: APPNAME,
+      viewSidebar: false,
+    };
+  },
+  methods: {
+    toggleSidebar() {
+      this.viewSidebar = !this.viewSidebar;
     },
-    data() {
-        return {
-            APPNAME: APPNAME,
-            viewSidebar: false,
-        }
-    },
-    methods: {
-        toggleSidebar () {
-            this.viewSidebar = !this.viewSidebar;
-        }
-    }
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .custom-height {
-    height: 100dvh;
+  min-height: 100dvh;
 }
 </style>
