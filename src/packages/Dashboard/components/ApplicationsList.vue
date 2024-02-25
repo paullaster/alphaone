b848e3756548daa635590a21954815508fc796d1
paullaster <paullasterokoth98@gmail.com>
2024-02-25 11:34:41 +0300

    add user payment options

--- src/packages/Dashboard/components/ApplicationsList.vue
+++ src/packages/Dashboard/components/ApplicationsList.vue
@@ -31,7 +31,7 @@
     </v-card>
    </v-container>
    <PaymentComponent :payment="paymentObject" :selectedCourse="selectedCourse" @initiateNIpusher="initiateNIPush"/>
-   <EditApplicationComponent :editApplication="applicationToEdit" :courses="coursesList" />
+   <EditApplicationComponent :editApplication="applicationToEdit" :courses="coursesList" @initiateUpdateApplication="updateApplication"/>
 </template>
 
 <script>
